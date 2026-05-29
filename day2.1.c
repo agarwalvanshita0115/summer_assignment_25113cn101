d44b6c816c3228bd0eb7107928e52f70acf7d1e7
agarwalvanshita0115 <agarwalvanshita3@gmail.com>
2026-05-29 17:57:58 +0530

    Add files via upload

---
+++ day2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+    printf("enter number:");
+    scanf("%d", &n);
+    while (n > 0)
+    {
+        int temp = n % 10;
+        sum = sum + temp;
+        n = n / 10;
+    }
+    printf("sum of the digit of given number:%d", sum);
+    return 0;
+}
---
+++ day2.3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, product = 1;
+    printf("enter number:");
+    scanf("%d", &n);
+    while (n > 0)
+    {
+
+        int temp = n % 10;
+        product = product * temp;
+        n = n / 10;
+    }
+    printf("product of digit:%d", product);
+
+    return 0;
+}

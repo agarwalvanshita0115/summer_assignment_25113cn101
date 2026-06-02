b460522697b2a12e317f8eb88631a6ec7a93f340
agarwalvanshita0115 <agarwalvanshita3@gmail.com>
2026-06-02 14:21:31 +0530

    Add files via upload

---
+++ day6/prob6.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int binary, decimal = 0, base = 1, rem;
+
+    printf("Enter binary number: ");
+    scanf("%d", &binary);
+
+    while(binary > 0) {
+        rem = binary % 10;
+        decimal += rem * base;
+        binary /= 10;
+        base *= 2;
+    }
+
+    printf("Decimal = %d", decimal);
+
+    return 0;
+}

b40e8568cb241f9d4d8f4390ff0cda08e1c06e94
agarwalvanshita0115 <agarwalvanshita3@gmail.com>
2026-06-22 10:44:57 +0530

    Add files via upload

---
+++ day 22/prob1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[1000],str1[1000];
+    printf("Enter your string value:");
+    scanf("%999s",str);
+    strcpy(str1,str);
+    strrev(str1);
+    if(strcmp(str1,str)==0){
+        printf("%s is a palindrome string.",str);
+    }
+    else{
+        printf("%s is not a palindrome string.",str);
+    }
+    return 0;
+}
